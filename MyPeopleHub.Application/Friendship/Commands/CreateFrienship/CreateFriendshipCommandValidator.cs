71a35417030b6d0b0d2a279ca71d9a13e8b2ca32
kamilus500 <kkurzeja321@gmail.com>
2023-12-26 21:35:52 +0100

    add validator

---
+++ MyPeopleHub.Application/Friendship/Commands/CreateFrienship/CreateFriendshipCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace MyPeopleHub.Application.Friendship.Commands.CreateFrienship
+{
+    public class CreateFriendshipCommandValidator : AbstractValidator<CreateFrienshipCommand>
+    {
+        public CreateFriendshipCommandValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty().WithMessage("User id is empty");
+
+            RuleFor(x => x.FriendId)
+                .NotEmpty().WithMessage("Friend id is empty");
+        }
+    }
+
+}
