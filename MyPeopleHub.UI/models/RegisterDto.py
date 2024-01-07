53350fba18c1a6281ef8519ddf9ef877116c6490
kamilus500 <kkurzeja321@gmail.com>
2024-01-07 20:07:00 +0100

    remove python project

--- MyPeopleHub.UI/models/RegisterDto.py
+++
@@ -1,7 +0,0 @@
-class RegisterDto:
-    def __init__(self, firstName, lastName, email, login, password):
-        self.FirstName = firstName
-        self.LastName = lastName
-        self.Email = email
-        self.Login = login
-        self.Password = password
