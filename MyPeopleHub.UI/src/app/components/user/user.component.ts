b79ecf1df4e7a491d1d3e916da880d5fdc6842bb
kamilus500 <kkurzeja321@gmail.com>
2024-02-10 14:48:51 +0100

    changes

--- MyPeopleHub.UI/src/app/components/user/user.component.ts
+++ MyPeopleHub.UI/src/app/components/user/user.component.ts
@@ -1,23 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { UserDataService } from '../../services/data-services/user-data.service';
 import { UserDto } from '../../models/UserDto';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export class UserComponent implements OnInit {
+export class UserComponent{
   user: UserDto|null = null;
+  user$: Observable<UserDto|null> = this.userDataService.userById$;
 
   constructor(private userDataService: UserDataService) {
     
-  }
-
-  ngOnInit(): void {
-    this.userDataService.userById$
-      .subscribe(user => {
-        this.user = user;
-      })
   }
 }
